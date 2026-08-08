85e9e6e7b33c9c200c8d3bed7546bf28f7dbfc73
umangkachhapati <umangkachhapati12345@gmail.com>
2026-08-09 00:12:31 +0530

    Update about.js

--- controller/about.js
+++ controller/about.js
@@ -1,8 +1,12 @@
 const about = (req, res) => {
-    try{
+    try {
         res.render('about');
-    } catch (error){
+    } catch (error) {
         console.log(error.message);
+        // Prevent the browser from hanging indefinitely if a rendering error occurs
+        res.status(500).send("Internal Server Error");
     }
 };
-export {about};
+
+// Use CommonJS export format
+module.exports = { about };
